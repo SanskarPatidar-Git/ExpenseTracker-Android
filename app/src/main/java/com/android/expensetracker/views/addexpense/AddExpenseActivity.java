162892e736acbd35512a074df5c23e32f8bdc9d5
Sanskar <sanskarpatidar14@gmail.com>
2024-04-26 01:11:20 +0530

    UI created of add expenses

---
+++ app/src/main/java/com/android/expensetracker/views/addexpense/AddExpenseActivity.java
@@ -0,0 +1,16 @@
+package com.android.expensetracker.views.addexpense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.expensetracker.R;
+
+public class AddExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+}
